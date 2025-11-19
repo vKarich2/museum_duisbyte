6a21698efb4b57ee11623b74bc6e71075812226b
vKarich2 <vladyslav.k17@gso.schule.koeln>
2025-11-19 12:46:39 +0100

    feat: add utils.py with simple enums

--- utils.py
+++ utils.py
@@ -1,1 +1,22 @@
 # utils.py
+
+
+
+# --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+# Hilfsklassen und -funktionen können hier definiert werden.
+# --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+
+
+
+from enum import IntEnum
+
+# Tickettypen
+class TicketType(IntEnum):
+		HALF_DAY = 1 		# Halber Tag
+		FULL_DAY = 2 		# Ganzer Tag
+
+# Mitgliedschaftstypen (ab 18 Jahre)
+class MembershipType(IntEnum):
+		PREMIUM = 1			# Premium (Sekt möglich)
+		BASIC = 2				# Basis-Mitgliedschaft
+		NONE = 3				# Keine Mitgliedschaft
